1b6b2a88ba962ef1df3e4ca45c94e751541b82c7
kazuya <ikazuya123@hotmail.co.jp>
2017-12-11 00:44:37 +0900

    不明バグ検証のためコミット

---
+++ Assets/MyWork/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+    private float side;
+    private float height;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.player = GameObject.Find("Player");
+        this.side = player.transform.position.x - this.transform.position.x;
+        this.height = player.transform.position.y - this.transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.player.transform.position.x - side, this.player.transform.position.y - height, this.transform.position.z);
+    }
+}
---
+++ Assets/MyWork/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //private Animator animator;
+    private Rigidbody rigid;
+    public float jumpFoce;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid = GetComponent<Rigidbody>();
+       // this.animator = GetComponent<Animator>();
+       // this.animator.SetFloat("FW", 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(0.005f, 0, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(-0.005f, 0, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.rigid.AddForce(transform.up * this.jumpFoce);
+        }
+    }
+}
